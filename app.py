058892eac9b0d77a94c301edf63a258c3356c58d
Harman1010 <140702065+Harman1010@users.noreply.github.com>
2026-05-21 13:38:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,77 +1,197 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import joblib
-import gradio as gr
 import pandas as pd
-model = joblib.load("model.joblib")
-label_encoder = joblib.load("label_encoder.joblib")
-imputerJoblib = joblib.load("imputer.joblib")
+from sentence_transformers import SentenceTransformer
+import numpy as np
+import faiss
 
-CONFIRMED_LABEL = label_encoder.transform(['CONFIRMED'])[0]
-THRESHOLD = 0.3
+app = FastAPI()
 
-FEATURE_NAMES = [
-    'koi_period',
-    'koi_time0bk',
-    'koi_duration',
-    'koi_depth',
-    'koi_impact',
-    'koi_model_snr',
-    'koi_prad',
-    'koi_teq',
-    'koi_insol',
-    'koi_steff'
-]
+model = joblib.load("finalModel.joblib")
+le = joblib.load("label_encoder.joblib")
+model_embed = SentenceTransformer('all-MiniLM-L6-v2')
+df = joblib.load("history_df.joblib")
+planet_texts = joblib.load("planet_texts.joblib")
+faiss_index = faiss.read_index("faiss_index.bin")
 
 
-def predict_signal(*inputs):
-
-    X = pd.DataFrame([inputs], columns=FEATURE_NAMES)
-
-    X_imputed = imputerJoblib.transform(X)
-
-    probs = model.predict_proba(X_imputed)[0]
-    prob_dict = dict(zip(label_encoder.classes_, probs))
-
-    p_confirmed = prob_dict['CONFIRMED']
-
-    if p_confirmed >= THRESHOLD:
-        decision = "🟢 SEND FOR REVIEW"
-        explanation = (
-            f"P(CONFIRMED) = {p_confirmed:.2f} ≥ {THRESHOLD}. "
-            "This signal should be reviewed by scientists."
-        )
+def engine(prob):
+    confidence = round(prob * 100, 2)
+    if confidence >= 70:
+        priority = "Critical"
+        review_status = "Immediate Review"
+    elif confidence >= 50:
+        priority = "High"
+        review_status = "Scientist Validation"
+    elif confidence >= 30:
+        priority = "Medium"
+        review_status = "Review Queue"
     else:
-        decision = "🔴 SKIP (LOW PRIORITY)"
-        explanation = (
-            f"P(CONFIRMED) = {p_confirmed:.2f} < {THRESHOLD}. "
-            "This signal is low priority for manual review."
-        )
+        priority = "Low"
+        review_status = "Auto-Filtered"
+    return {
+        "Confidence Score": confidence,
+        "Priority": priority,
+        "Review Status": review_status
+    }
 
-    return (
-        decision,
-        explanation,
-        prob_dict
-    )
-inputs = [
-    gr.Number(label=feature) for feature in FEATURE_NAMES
-]
+def create_input_signal(period,depth,prad,steff):
+  text = f"""
+    Orbital Period: {'koi_period'},
+    Transit Depth: {'koi_depth'},
+    Planet Radius: {'koi_prad'},
+    Stellar Temperature: {'koi_steff'}
+    """
+  return text
 
-outputs = [
-    gr.Text(label="Decision"),
-    gr.Text(label="Explanation"),
-    gr.JSON(label="Class Probabilities")
-]
+def retrieve_similar_planets(input_text,k=3):
 
-app = gr.Interface(
-    fn=predict_signal,
-    inputs=inputs,
-    outputs=outputs,
-    title="Kepler Exoplanet Signal Review System",
-    description=(
-        "This application uses a trained ML model to prioritize Kepler "
-        "telescope signals for human review. A probability threshold of 0.3 "
-        "was selected offline to reduce manual workload while retaining most "
-        "confirmed planets."
-    ),
-)
+    query_embedding = model_embed.encode([input_text])
 
-app.launch()
+    distances,indices = faiss_index.search(np.array(query_embedding).astype('float32'),k)
+
+    results = []
+
+    for idx in indices[0]:
+
+        idx = int(idx)
+        results.append({
+            "Planet Name": str(df.iloc[idx]['kepoi_name']),
+
+            "Planet Summary": str(planet_texts[idx]),
+
+            "Disposition": le.inverse_transform([int(df.iloc[idx]['koi_disposition'])])[0]
+
+        })
+
+    return results
+
+class ValidationRequest(BaseModel):
+    koi_period: float
+    koi_time0bk: float
+    koi_duration: float
+    koi_depth: float
+    koi_impact: float
+    koi_model_snr: float
+    koi_prad: float
+    koi_teq: float
+    koi_insol: float
+    koi_steff: float
+
+class HistoryRequest(BaseModel):
+    
+        koi_period: float
+
+        koi_depth: float
+
+        koi_prad: float
+
+        koi_steff: float
+
+
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to Astronomy with FastAPI!"}
+
+@app.get("/example")
+def example():
+
+    return {
+
+        "predict_example": {
+
+            "url": "/predict",
+
+            "method": "POST",
+
+            "sample_input": {
+
+                "koi_period": 0.5,
+                "koi_time0bk": 100.0,
+                "koi_duration": 2.5,
+                "koi_depth": 120.0,
+                "koi_impact": 0.3,
+                "koi_model_snr": 15.0,
+                "koi_prad": 1.1,
+                "koi_teq": 500.0,
+                "koi_insol": 1.2,
+                "koi_steff": 5500.0
+
+            }
+
+        },
+
+        "history_example": {
+
+            "url": "/history",
+
+            "method": "POST",
+
+            "sample_input": {
+
+                "koi_period": 0.5,
+                "koi_depth": 120.0,
+                "koi_prad": 1.1,
+                "koi_steff": 5500.0
+
+            }
+
+        }
+
+    }
+
+@app.post("/predict")
+def predict(data: ValidationRequest):
+    input_df = pd.DataFrame([{
+        "koi_period": data.koi_period,
+        "koi_time0bk": data.koi_time0bk,
+        "koi_duration": data.koi_duration,
+        "koi_depth": data.koi_depth,
+        "koi_impact": data.koi_impact,
+        "koi_model_snr": data.koi_model_snr,
+        "koi_prad": data.koi_prad,
+        "koi_teq": data.koi_teq,
+        "koi_insol": data.koi_insol,
+        "koi_steff": data.koi_steff
+    }])
+    
+    prediction = model.predict(input_df)[0]
+
+    prediction_label = le.inverse_transform([prediction])[0]
+
+    probabilities = model.predict_proba(input_df)[0]
+
+    confirmed_idx = list(le.classes_).index("CONFIRMED")
+
+    confirmed_prob = float(probabilities[confirmed_idx])
+
+    review_data = engine(confirmed_prob)
+
+    return {
+
+        "predicted_class": prediction_label,
+
+        "Confidence Score": review_data["Confidence Score"],
+
+        "Priority": review_data["Priority"],
+
+        "Review Recommendation": review_data["Review Status"]
+
+    }
+
+@app.post("/history")
+def history(data : HistoryRequest):
+    query_text = create_input_signal(data.koi_period,data.koi_depth,data.koi_prad,data.koi_steff)
+
+    similar_planets = retrieve_similar_planets(query_text,k=3)
+
+    return {
+
+        "Historical Similar Planets": similar_planets
+
+    }
+
+
+        
